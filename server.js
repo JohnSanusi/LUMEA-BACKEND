aa76a8f791033d473037929a08a7ea0d080ecc49
JohnSanusi <johnsanusi18@gmail.com>
2025-09-26 18:15:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use("/api/payment", payment);
 app.use("/api/contact", contact);
 
