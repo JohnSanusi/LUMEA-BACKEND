d45492a87c5abdf364cd9f65b71362d9b647da40
JohnSanusi <johnsanusi18@gmail.com>
2025-09-27 03:35:07 +0100

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: BREVO_USER,
     pass: BREVO_PASSWORD,
@@ -43,6 +43,7 @@ router.post("/", async (req, res) => {
 
 
 export default router;
+
 
 
 
