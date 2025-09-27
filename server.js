390593d6b116c7f63b9c718f161eb2e70a1d1c12
JohnSanusi <johnsanusi18@gmail.com>
2025-09-27 05:07:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import payment from "./routes/payment.js";
 import contact from "./routes/contact.js";
+import test from "./routes/test.js";
 import { PORT } from "./config/env.js";
 
 const app = express();
@@ -25,6 +26,7 @@ app.use(
 
 app.use(express.json());
 app.use("/api/payment", payment);
+app.use("/api/test-brevo", test);
 app.use("/api/contact", contact);
 
 app.listen(PORT, async () => {
